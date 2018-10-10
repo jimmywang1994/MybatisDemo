53c8803bd2990aa7204457f232f6bdf9f3756512
wangwei <429744781@qq.com>
2018-10-10 15:48:24 +0800

    搭建基础mybatis+springmvc项目

---
+++ src/main/java/ww/school/model/Student.java
@@ -0,0 +1,42 @@
+package ww.school.model;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int no;
+    private String className;
+    private String major;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
